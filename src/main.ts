b2060ac0e14ee6d0040844f7b67535f2e7c94ec2
Emersonk24 <emerpachon206@gmail.com>
2025-10-05 23:20:30 -0500

    se actualiza la main con cambios de la importacion de archivo del ejercicio 7

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import './bases/02-template-string'
 import './bases/03-object-literal'
 import './bases/04-arrays'
 import './bases/05-functions'
+import './bases/07-array-destructuring'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
